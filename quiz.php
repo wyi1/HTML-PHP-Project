5079e3e470aee65e4c86af9f3b1426d6c23c5e31
zling0531 <67967784+zling0531@users.noreply.github.com>
2020-07-12 01:58:12 +0800

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,87 +1,125 @@
-<script type="text/javascript">
-// pos is position of where the user in the test or which question they're up to
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
-// this is a multidimensional array with 4 inner array elements with 5 elements inside them
-var questions = [
-  {
-      question: "What is Dr. Koh's full name?",
-      a: "KOH SIEW HUA",
-      b: "KOH TIENG WEI",
-      c: "ALVIN KOH",
-      answer: "B"
-    },
-  {
-      question: "Why do Dr teach?",
-      a: "Because he get paid a lot of money.",
-      b: "Because he likes having holidays.",
-      c: "Because he loves the students he teaches.",
-      answer: "C"
-    },
-  {
-      question: "When is the normal class time?",
-      a: "Monday 12pm to 2 pm",
-      b: "Friday 8pm to 10pm",
-      c: "Wednesday 5pm to 7pm",
-      answer: "A"
-    },
-  {
-      question: "Which software is using during class?",
-      a: "Google meet",
-      b: "Zoom",
-      c: "Cisco Webex Meetings",
-      answer: "C"
-    }
-  ];
-// this get function is short for the getElementById function
-function get(x){
-  return document.getElementById(x);
-}
-// this function renders a question for display on the page
-function renderQuestion(){
-  test = get("test");
-  if(pos >= questions.length){
-    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
-    // resets the variable to allow users to restart the test
-    pos = 0;
-    correct = 0;
-    // stops rest of renderQuestion function running when test is completed
-    return false;
-  }
-  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+<!DOCTYPE html>
+<!--code by webdevtrick (webdevtrick.com) -->
+<head>
+	<meta charset=UTF-8 />
+	<title>SSE3150 Quiz</title>
+</head>
 
-  question = questions[pos].question;
-  chA = questions[pos].a;
-  chB = questions[pos].b;
-  chC = questions[pos].c;
-  // display the question
-  test.innerHTML = "<h3>"+question+"</h3>";
-  // display the answer options
-  // the += appends to the data we started on the line above
-  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
-  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-}
-function checkAnswer(){
-  // use getElementsByName because we have an array which it will loop through
-  choices = document.getElementsByName("choices");
-  for(var i=0; i<choices.length; i++){
-    if(choices[i].checked){
-      choice = choices[i].value;
-    }
-  }
-  // checks if answer matches the correct choice
-  if(choice == questions[pos].answer){
-    //each time there is a correct answer this value increases
-    correct++;
-  }
-  // changes position of which character user is on
-  pos++;
-  // then the renderQuestion function runs again to go to next question
-  renderQuestion();
-}
-// Add event listener to call renderQuestion on page load event
-window.addEventListener("load", renderQuestion);
+<body>
+	<div id="page-wrap">
+	<h1>Simple Quiz Built On PHP</h1>
 
-</script>
+	<form action="result.php" method="post" id="quiz">
+    <ol>
+      <li>
+        <h3>What is Dr. Koh's full name?</h3>
+
+        <div>
+          <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+          <label for="question-1-answers-A">KOH SIEW HUA</label>
+        </div>
+                        
+        <div>
+          <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+          <label for="question-1-answers-B">KOH TIENG WEI</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+          <label for="question-1-answers-C">ALVIN KOH</label>
+        </div>
+
+      </li>
+
+      <li>
+        <h3>Why do Dr. teach us?</h3>
+
+        <div>
+      <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+      <label for="question-2-answers-A">Because he get paid a lot of money.</label>
+      </div>
+
+      <div>
+        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+        <label for="question-2-answers-B">Because he likes having holidays.</label>
+      </div>
+
+      <div>
+        <input type="radio" name="question-1-answers" id="question-2-answers-C" value="C" />
+        <label for="question-2-answers-C">Because he loves the students he teaches.</label>
+      </div>
+
+      </li>
+
+      <li>
+        <h3>When is the normal class time?</h3>
+
+        <div>
+          <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+          <label for="question-1-answers-A">Monday 12pm to 2 pm</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+          <label for="question-3-answers-B">Friday 8pm to 10pm</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+          <label for="question-3-answers-C">Wednesday 5pm to 7pm</label>
+        </div>
+
+      </li>
+
+      <li>
+        <h3>Which software is using during class?</h3>
+
+        <div>
+          <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+          <label for="question-4-answers-A">Google Meet</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+          <label for="question-4-answers-B">Zoom Meeting</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+          <label for="question-4-answers-C">Cisco Webex Meeting</label>
+        </div>
+
+      </li>
+
+      <li>
+        <h3>Where did Dr. upload the lectures' recording?</h3>
+
+        <div>
+          <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+          <label for="question-5-answers-A">Google Drive</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+          <label for="question-5-answers-B">Youtube</label>
+        </div>
+
+        <div>
+          <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+          <label for="question-5-answers-C">Facebook</label>
+        </div>
+
+      </li>
+
+    </ol>
+
+    <input type="submit" value="Submit" class="submitbtn" />
+
+		</form>
+
+	</div>
+
+
+</body>
+
+</html>
