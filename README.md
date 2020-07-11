0109e43d8acdba10fb9abf690d4758c2a5ee557a
wyi1 <jamie.wenyi@gmail.com>
2020-07-11 16:04:58 +0800

    modify login

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ To pull: -
 1. git pull
 
 try to use git pull before push to avoid any missing data from your friend if they got push
+
+you work on this atom in this directory C:\xampp\htdocs\SSE3150-Project. then u can straight push and pull without copy here and there
--- login.php
+++ login.php
@@ -11,39 +11,55 @@
     <title>Login</title>
 
         <body>
-        <form method = "POST" action="loginaction.php">
           <h1>Login to Play</h1>
-          <?php
-          $salt = 'XyZzy12*_';
-          $stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';
-          $md5 = hash('md5', 'XyZzy12*_php123');
+
+<?php
+include 'db_connection.php';
+$conn = OpenCon();
+$pdo=new PDO('mysql:host=localhost;port=3306;dbname=project',
+     'root', '1234');
+
 
           if ( isset($_POST['uname']) && isset($_POST['pw']) ) {
-              if ( strlen($_POST['uname']) < 1 || strlen($_POST['pw']) < 1 ) {
+            $uname = $_POST['uname'];
+            $pw = $_POST['pw'];
+            $stored_hash = md5($_POST['pw']);
+              if ( strlen($uname) < 1 || strlen($pw) < 1 ) {
                 $msg = "User name and password are required";
                 } else {
-                  $check = hash('md5', $salt.$_POST['pw']);
-                    if ( $check == $stored_hash ) {
-                      header("Location: quiz.php?name=".urlencode($_POST['uname']));
+                  $query1 = "SELECT count(*) FROM user WHERE username='$uname'";
+                  $result1 = $pdo->query($query1);
+
+                  if($result1->rowCount() == 1){
+                    $query2 = "SELECT password FROM user WHERE username='$uname'";
+                    $result2 = $pdo->prepare($query2);
+                    //echo $result2; //Testing to see if am getting the hashed password.
+                    if(password_verify($stored_hash, $result2 )){
+
+                      header("Location: quiz.php?name=".urlencode($uname));
                       return;
                     } else {
                       $msg = "Incorrect password";
                     }
-                }
+                    if ( $msg !== false ) {
+                      echo('<p style="color: red;">'.htmlentities($msg)."</p>\n");
 
-                if ( $msg !== false ) {
-                  echo('<p style="color: red;">'.htmlentities($msg)."</p>\n");
-        }
-        }
+                    }
+                }else if ($result1->rowCount() == 0) {
+                  echo "User does not exist, please redirect to register" ?> <a href="register.php">Sign Up</a> <?php ;
+                }
+}
+
+      }
         if ( isset($_POST['register']) ) {
          header('Location: register.php');
          return;
         }
-           ?>
+
+
+?>
            <form method="post">
              <p>User Name <input type="text" name="uname"/></p>
               <p>Password <input type="password" name="pw"/></p>
-             <p><button type="submit">Log In</button><button type="button" name="register">Sign Up/button></p>
+             <p><button type="submit">Log In</button><button type="submit" name="register">Sign Up</button></p>
            </form>
-        </body>
-        </html>
